c98cc525a6e1a4a31bc4b24a637e0978200f8490
browne8 <aishabrowne@resnet-14-22.resnet.ucsb.edu>
2013-06-01 14:45:29 -0700

    Addition of radio buttons
    Radio buttons and Toast windows for menu buttons

--- gen/edu/ucsb/cs/cs185/FoodTracker/R.java
+++ gen/edu/ucsb/cs/cs185/FoodTracker/R.java
@@ -20,27 +20,44 @@ public final class R {
         public static final int green_plus_md=0x7f020000;
     }
     public static final class id {
-        public static final int SevenDayTotalTag=0x7f070004;
-        public static final int SevenDayTotals=0x7f070005;
-        public static final int button=0x7f070003;
-        public static final int homeList=0x7f070002;
-        public static final int homeListContainer=0x7f070001;
-        public static final int mainLayout=0x7f070000;
-        public static final int menu_Create=0x7f070007;
-        public static final int menu_info=0x7f070006;
+        public static final int SevenDayTotalTag=0x7f07000b;
+        public static final int SevenDayTotals=0x7f07000c;
+        public static final int button=0x7f07000a;
+        public static final int create_info=0x7f07000d;
+        public static final int create_title=0x7f070000;
+        public static final int edit_name=0x7f070006;
+        public static final int homeList=0x7f070009;
+        public static final int homeListContainer=0x7f070008;
+        public static final int mainLayout=0x7f070007;
+        public static final int menu_Create=0x7f070010;
+        public static final int menu_add=0x7f07000e;
+        public static final int menu_info=0x7f07000f;
+        public static final int name_meal=0x7f070005;
+        public static final int rad_brkfast=0x7f070002;
+        public static final int rad_dinner=0x7f070004;
+        public static final int rad_lunch=0x7f070003;
+        public static final int radio_meal=0x7f070001;
     }
     public static final class layout {
         public static final int create_meal=0x7f030000;
         public static final int main=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f060000;
+        public static final int cmealmenu=0x7f060000;
+        public static final int main=0x7f060001;
     }
     public static final class string {
         public static final int add_meal=0x7f050001;
+        public static final int add_menu=0x7f050008;
         public static final int app_name=0x7f050000;
+        public static final int breakfast=0x7f050009;
         public static final int create_menu=0x7f050003;
+        public static final int create_title=0x7f050005;
+        public static final int dinner=0x7f05000b;
+        public static final int edit_name=0x7f050007;
         public static final int info_menu=0x7f050002;
+        public static final int lunch=0x7f05000a;
+        public static final int name_meal=0x7f050006;
         public static final int title_activity_create_meal=0x7f050004;
     }
 }
--- src/edu/ucsb/cs/cs185/FoodTracker/CreateAMealActivity.java
+++ src/edu/ucsb/cs/cs185/FoodTracker/CreateAMealActivity.java
@@ -3,6 +3,8 @@ package edu.ucsb.cs.cs185.FoodTracker;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class CreateAMealActivity extends Activity{
 	protected void onCreate(Bundle savedInstanceState) {
@@ -14,9 +16,22 @@ public class CreateAMealActivity extends Activity{
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.cmealmenu, menu);
 		return true;
 	}
 	
+	 public boolean onOptionsItemSelected(MenuItem item){
+		 switch(item.getItemId()){
+		 case R.id.create_info:
+			 Toast.makeText(this, "Create a new meal here! It will be saved to you list of meals and can be used later.", Toast.LENGTH_SHORT).show();
+			 break;
+		 case R.id.menu_add:
+			 Toast.makeText(this, "Go back to Add Meal Fragment", Toast.LENGTH_SHORT).show();
+			 break;
+		default:
+			break;
+		 }
+		 return true;
+	 }
 
 }
