eaf95a10aa37cf88c7c748fd0fef7eb86b130d23
browne8 <aishabrowne@resnet-14-22.resnet.ucsb.edu>
2013-06-02 15:02:53 -0700

    Create Meal Page
    Added the rest of screen.
    Done button returns to page that called it

--- src/edu/ucsb/cs/cs185/FoodTracker/CreateAMealActivity.java
+++ src/edu/ucsb/cs/cs185/FoodTracker/CreateAMealActivity.java
@@ -4,14 +4,24 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
+
+
 public class CreateAMealActivity extends Activity{
+
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.create_meal);
+		setContentView(R.layout.create_meal);	
 	}
 	
+	
+	public void OnClickDone(View v)
+	{
+		finish();
+	}
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
