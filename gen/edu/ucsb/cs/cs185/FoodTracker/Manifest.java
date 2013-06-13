146016713802ea25c8de096037fbe26800d73c82
peterwilson <vexxer91@gmail.com>
2013-06-13 14:04:33 -0700

    readded gen/

---
+++ gen/edu/ucsb/cs/cs185/FoodTracker/Manifest.java
@@ -0,0 +1,5 @@
+package edu.ucsb.cs.cs185.FoodTracker;
+
+/* This stub is for using by IDE only. It is NOT the Manifest class actually packed into APK */
+public final class Manifest {
+}
---
+++ gen/edu/ucsb/cs/cs185/FoodTracker/R.java
@@ -0,0 +1,5 @@
+package edu.ucsb.cs.cs185.FoodTracker;
+
+/* This stub is for using by IDE only. It is NOT the R class actually packed into APK */
+public final class R {
+}
